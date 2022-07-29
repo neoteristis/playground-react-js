ef70b7d8c00443b2ca1002041453c9cefae86aaa
neoteristis <anjou.raphael@gmail.com>
2022-07-29 09:31:27 +0200

    feat: board is now created using two for loops

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -2,28 +2,27 @@ import React from "react";
 import {Square} from "../Square";
 
 class Board extends React.Component {
+
     renderSquare(i) {
         return <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>;
+    }
+
+    createBoard() {
+        let output_array = [];
+        for (let i = 0; i < 3; i++) {
+            let temp_array = [];
+            for (let j = 0; j < 3; j++) {
+                temp_array.push(this.renderSquare(j + 3*i))
+            }
+            output_array.push(<div className="board-row">{temp_array}</div>)
+        }
+        return output_array
     }
 
     render() {
         return (
             <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
+                {this.createBoard()}
             </div>
         );
     }
